f470c1ed55e0d4ae0dfaf4f2e812a6e547ee973e
Sophia6116 <sophiamirandadossantos61@gmail.com>
2026-01-13 21:56:41 -0300

    20-Fazendo responsividade e adcionando hamburguer

---
+++ src/js/sobrenos.js
@@ -0,0 +1,14 @@
+const hamburguer = document.querySelector('.hamburguer');
+const headerMenu = document.querySelector('.menu-header');
+
+function toggleMenu(){
+    hamburguer.classList.toggle('active');
+    headerMenu.classList.toggle('active');
+}
+
+hamburguer.addEventListener('click', toggleMenu);
+headerMenu.addEventListener('click', (event) => {
+    if (event.target.classList.contains('item-menu')) {
+        toggleMenu();
+    }
+});
